b40d80c95e4ecd66029b602aae5c24fbab687563
solar22 <alexander@gruessung-online.de>
2013-05-07 11:37:24 +0200

    config file erstellt

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+	/*
+	 * This is the config file.
+	 * Be careful!
+	 */
+	 
+	 //Database settings
+	 define("sqlite", "database.sqlite");
+	 
+	 //Cache Settings (if true, gBooking will cache data in sqlite database)
+	 define ("cache", false);
+	 
+	 //Spreadsheet link (be sure the document is public!) // Insert the FULL url!
+	 //e.g. https://docs.google.com/spreadsheet/ccc?key=0Alf1vrd7tNnTdE9vRnljWkszNFpjRmtCdmlPalJQd2c#gid=0
+	 define("spreadsheet", "https://docs.google.com/spreadsheet/ccc?key=0Alf1vrd7tNnTdE9vRnljWkszNFpjRmtCdmlPalJQd2c#gid=0");
+	 
+	 //Google Maps v3 API
+	 define("apikey", "AIzaSyCxNQZmYsOTkVzXgwR6CkQuzVfaioAqY2w"); //AIzaSyCxNQZmYsOTkVzXgwR6CkQuzVfaioAqY2w
+	 
+	 
+	
