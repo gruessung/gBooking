6326a36bcbac43cb32e703985f2b21238c7e3feb
solar22 <alexander@gruessung-online.de>
2013-05-08 13:41:15 +0200

    header verändert

--- style/header.php
+++ style/header.php
@@ -10,45 +10,7 @@
 	<!-- JavaScript files -->
 	<script src="style/js/jquery.js"></script>
     <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=<?=apikey?>&sensor=false"></script>
-    <script type="text/javascript">
-      
-	var map;
 
-	function initialize() {
-	  var mapOptions = { center: new google.maps.LatLng(0.0, 0.0), 
-	                     zoom: 2,
-	                     mapTypeId: google.maps.MapTypeId.ROADMAP };
-	  map = new google.maps.Map(document.getElementById("gmap"), mapOptions);
-	  
-	  var arraylng = [0, 0, 0, 0, 0];
-	  var arraylat = [0, 10, 20, 30, 40];
-	
-	  var markers = [];
-	
-	  var infowindow = new google.maps.InfoWindow();
-	
-	  for (var i = 0; i < arraylng.length; i++) {
-	    var marker = new google.maps.Marker({
-	      position: new google.maps.LatLng(arraylng[i], arraylat[i]),
-	      map: map
-	    });
-	
-	    makeInfoWindowEvent(map, infowindow, "test" + i, marker);
-	    
-	    markers.push(marker);
-	  }
-	}
-	
-	function makeInfoWindowEvent(map, infowindow, contentString, marker) {
-	  google.maps.event.addListener(marker, 'click', function() {
-	    infowindow.setContent(contentString);
-	    infowindow.open(map, marker);
-	  });
-	  
-	  $("#status").html("Karte geladen");
-	}
-	google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
 
 
     <!-- CSS files -->
