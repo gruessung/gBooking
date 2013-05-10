18970450be5e26a2f6504caca98c971a35bce227
solar22 <alexander@gruessung-online.de>
2013-05-10 21:55:28 +0200

    header geändert für datepicker

--- style/header.php
+++ style/header.php
@@ -10,11 +10,12 @@
 	<!-- JavaScript files -->
 	<script src="style/js/jquery.js"></script>
     <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=<?=apikey?>&sensor=false"></script>
-
+	<script src="style/js/jquery-ui.js"></script>
 
 
     <!-- CSS files -->
    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+   <link href='style/css/jquery-ui.css' rel='stylesheet' type='text/css'>
    <style>
    	body
    	{
@@ -22,16 +23,22 @@
    	}
    </style>
 
-
+   <script>
+	  $(function() {
+	    $( "#datepicker" ).datepicker();
+	  });
+   </script>
 
   
   </head>
 
   <body>
-   	<div id="header" style="height:50px;">
+   	<div id="header" style="height:80px;">
    		<center>
-   			<span style="font-size: 20pt;">gBooking</span>
-   			<span style="float:right;" id="status">Lade Daten...</span>	
+   			<span style="font-size: 20pt;">
+   				gBooking<br>
+   				<form action="index.php" method="GET"><input name="date" type="text" id="datepicker" size="30" value="<?=@$_GET["date"]?>" /><input type="submit" value="anzeigen" /></form>
+   			</span>
    		</center>
    		
    	</div>  
